4a30e968143281ff15a3728af692c8a23bc5a247
Alex Cooksey <aecooksey2651@gmail.com>
2022-09-01 17:22:20 -0400

    Basic habit card and goal card page formatting

--- client/src/pages/Goals.tsx
+++ client/src/pages/Goals.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import { Container, Card, Button } from 'react-bootstrap';
+import GoalCard from '../components/goals/GoalCard';
 
 const Goals = () => {
-  const goals = [];
+  const goals = [{id: 1, goal_description: "I want to lose weight", completed: true}];
+  
+  const activeGoals = goals.filter(goal => goal.completed === false)
+  const activeGoalCards = activeGoals.map(goal => <GoalCard key={goal.id} goal={goal} />)
+
+  const completedGoals = goals.filter(goal => goal.completed === true)
+  const completedGoalCards = completedGoals.map(goal => <GoalCard key={goal.id} goal={goal} />)
 
   return (
-    <div>
-      {(goals.length > 0) ? 
-      <p>hello</p> : 
-      <p>You haven't set any goals</p>}
+    <div id="goal-card-page">
+      <Container id="active-goal-cards">
+        <h1>Current</h1>
+        {activeGoalCards.length > 0 ? activeGoalCards : <p>You don't have any current goals</p>}
+      </Container>
+      <Container id="inactive-habit-cards">
+        <h1>Previously Completed Goals</h1>
+        {completedGoalCards.length > 0 ? completedGoalCards : <p>You don't have any completed goals</p>}
+      </Container>
     </div>
   )
 }
--- client/src/pages/Habits.tsx
+++ client/src/pages/Habits.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
-import WeeklyCheckbox from '../components/habits/WeeklyCheckbox';
+import { Container, Card, Button } from 'react-bootstrap';
+import HabitCard from '../components/habits/HabitCard';
 
 const Habits = () => {
-  const habits = [];
-  if (habits.length < 1) return (
-    <div>
-      <p>You haven't set any habits</p>
-      <WeeklyCheckbox />
-    </div>
-  )
+  const habits = [{id: 1, habit_description: "I want to eat more vegetables", is_active: true}];
+  
+  const activeHabits = habits.filter(habit => habit.is_active === true)
+  const activeHabitCards = activeHabits.map(habit => <HabitCard key={habit.id} habit={habit} />)
+
+  const inactiveHabits = habits.filter(habit => habit.is_active === false)
+  const inactiveHabitCards = inactiveHabits.map(habit => <HabitCard key={habit.id} habit={habit} />)
+
   return (
-    <div>
-      {(habits.length > 0) ? 
-      <p>hello</p> : 
-      <p>You haven't set any habits</p>}
+    <div id="habit-card-page">
+      <Container id="active-habit-cards">
+        <h1>Active Habits</h1>
+        {activeHabitCards.length > 0 ? activeHabitCards : <p>You don't have any active habits</p>}
+      </Container>
+      <Container id="inactive-habit-cards">
+        <h1>Inactive Habits</h1>
+        {inactiveHabitCards.length > 0 ? inactiveHabitCards : <p>You don't have any inactive habits</p>}
+      </Container>
     </div>
   )
 }
