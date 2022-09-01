df26eeb1a2e651ab33524b71676ca2d20a042445
Alex Cooksey <aecooksey2651@gmail.com>
2022-09-01 09:50:01 -0400

    Add create, destroy, update, mark_complete methods to goals controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -2,7 +2,43 @@ class GoalsController < ApplicationController
 rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
 
     before_action :authorize
+    
+    def create
+        user = User.find_by(id: session[:user_id])
+        new_goal = user.goals.create!(habit_params)
+        new_goal.completed = false
+        render json: new_goal, status: :created
+    end
 
+    def destroy
+        goal = Goal.find_by(id: params[:id])
+        if goal
+            goal.destroy
+            head :no_content
+        else
+            render json: {error: "Goal record not found"}, status: :not_found
+        end
+    end
+
+    def update
+        goal = Goal.find_by(id: params[:id])
+        if goal
+            goal.update!(goal_params)
+            render json: goal, status: :ok
+        else
+            render json: {error: "Goal record not found"}, status: :not_found
+        end
+    end
+
+    def mark_complete
+        goal = Goal.find_by(id: params[:id])
+        if goal
+            goal.update!(completed: true)
+            render json: goal, status: :ok
+        else
+            render json: {error: "Goal record not found"}, status: :not_found
+        end
+    end
     private 
 
     def authorize
